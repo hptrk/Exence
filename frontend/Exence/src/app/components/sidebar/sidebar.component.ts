3c0b7284f54060771364d711a85562c0327db510
Horánszki Patrik <horanszkipatrik@gmail.com>
2025-01-09 17:14:30 +0100

    finish sidebar styling, placement

--- frontend/Exence/src/app/components/sidebar/sidebar.component.ts
+++ frontend/Exence/src/app/components/sidebar/sidebar.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 import { ThemeService } from '../../services/theme.service';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [RouterModule],
+  imports: [
+    RouterModule,
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule,
+    MatButtonModule,
+  ],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss',
 })
