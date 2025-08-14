57d8210d5fb8bbb224e6dbadb00f1df39ac315a0
Tamás Nagy <ntamasalbert@gmail.com>
2025-08-14 15:29:44 +0200

    Add eslint configs
    Added:
    * .ts file order
    * error on no function return type given
    * .ts file indentation to just tabs
    * some async await, then promise things
    * no const required in for of loop
    * unnecessary assertion and condition errors
    * now strings require single quotes
    * import order
    * not using await on an async functions call now throws an error

--- frontend/Exence/eslint.config.js
+++ frontend/Exence/eslint.config.js
@@ -14,89 +14,147 @@ module.exports = tseslint.config(
     ],
     processor: angular.processInlineTemplates,
     rules: {
-      "@angular-eslint/directive-selector": [
-        "error",
-        {
-          type: "attribute",
-          prefix: "ex",
-          style: "camelCase",
-        },
-      ],
-      "@angular-eslint/component-selector": [
-        "error",
-        {
-          type: "element",
-          prefix: "ex",
-          style: "kebab-case",
-        },
-      ],
-      // TS naming conventions
-      "@typescript-eslint/naming-convention": [
-        "error",
-        {
-          selector: "enumMember",
-          format: ["UPPER_CASE"],
-        },
-        {
-          selector: ["classProperty"],
-          format: ["PascalCase"],
-          filter: {
-            regex: ".*Enum",
-            match: true,
-          },
-        },
-        {
-          selector: ["classProperty"],
-          format: ["UPPER_CASE"],
-          modifiers: ["readonly"],
-        },
-        {
-          selector: ["classProperty"],
-          format: ["UPPER_CASE"],
-          modifiers: ["static"],
-        },
-        {
-          selector: ["variable", "classProperty", "method"],
-          format: ["camelCase"],
-        },
-        {
-          selector: ["variable"],
-          format: ["camelCase", "PascalCase", "UPPER_CASE"],
-          modifiers: ["const", "exported"],
-        },
-      ],
-      "@typescript-eslint/consistent-type-definitions": ["error", "interface"],
-      "@typescript-eslint/consistent-type-assertions": [
-        "error",
-        {
-          assertionStyle: "as",
-          objectLiteralTypeAssertions: "allow-as-parameter",
-        },
-      ],
-      "@typescript-eslint/dot-notation": "off",
-      "@typescript-eslint/explicit-member-accessibility": "off",
+		"@typescript-eslint/dot-notation": "off",
+        "@typescript-eslint/array-type": "off",
+		"@typescript-eslint/explicit-member-accessibility": "off",
+        "@typescript-eslint/no-unsafe-function-type": "error",
+        "@typescript-eslint/no-wrapper-object-types": "error",
 
-      // JS base rules
-      "brace-style": ["error", "1tbs"],
-      "id-blacklist": "off",
-      "id-match": "off",
-      "max-len": [
-        "error",
-        {
-          ignorePattern: "^import [^,]+ from |^export | implements",
-          code: 150,
-        },
-      ],
-      "no-underscore-dangle": "off",
-      "jsdoc/newline-after-description": "off",
-    },
-  },
-  {
-    files: ["**/*.html"],
-    extends: [
-      ...angular.configs.templateRecommended,
-      ...angular.configs.templateAccessibility,
-    ],
-    rules: {},
-  }
+
+		"@angular-eslint/directive-selector": [
+			"error",
+			{
+				type: "attribute",
+				prefix: [],
+				style: "camelCase",
+			},
+		],
+		"@angular-eslint/component-selector": [
+			"error",
+			{
+				type: "element",
+				prefix: "ex",
+				style: "kebab-case",
+			},
+		],
+		// TS naming conventions
+		"@typescript-eslint/naming-convention": [
+			"error",
+			{
+				selector: "enumMember",
+				format: ["UPPER_CASE"],
+			},
+			{
+				selector: ["classProperty"],
+				format: ["PascalCase"],
+				filter: {
+					regex: ".*Enum",
+					match: true,
+				},
+			},
+			{
+				selector: ["classProperty"],
+				format: ["UPPER_CASE"],
+				modifiers: ["readonly"],
+			},
+			{
+				selector: ["classProperty"],
+				format: ["UPPER_CASE"],
+				modifiers: ["static"],
+			},
+			{
+				selector: ["variable", "classProperty", "method"],
+				format: ["camelCase"],
+			},
+			{
+				selector: ["variable"],
+				format: ["camelCase", "PascalCase", "UPPER_CASE"],
+				modifiers: ["const", "exported"],
+			},
+		],
+		"@typescript-eslint/explicit-function-return-type": [
+          "error",
+          {
+            "allowExpressions": true,
+            "allowTypedFunctionExpressions": true,
+            "allowHigherOrderFunctions": true,
+            "allowDirectConstAssertionInArrowFunctions": true,
+            "allowConciseArrowFunctionExpressionsStartingWithVoid": false
+          }
+        ],
+		"@typescript-eslint/consistent-type-definitions": ["error", "interface"],
+		"@typescript-eslint/consistent-type-assertions": [
+			"error",
+			{
+				assertionStyle: "as",
+				objectLiteralTypeAssertions: "allow-as-parameter",
+			},
+		],
+		"@typescript-eslint/indent": [
+			"off",
+			"tab"
+        ],
+		"@typescript-eslint/member-ordering": [
+			"warn",
+			{
+				"default": [],
+				"classes": [["field", "get", "set"], "constructor", "instance-method"]
+			}
+			],
+		"@typescript-eslint/prefer-const": [
+			"error",
+			{
+				"destructuring": "any",
+				"ignoreReadBeforeAssign": false
+			}
+		],
+		"@typescript-eslint/await-thenable": "error",
+		"@typescript-eslint/typedef": "error",
+		"@typescript-eslint/no-for-in-array": "error",
+        "@typescript-eslint/no-this-alias": "error",
+        "@typescript-eslint/no-unnecessary-boolean-literal-compare": "error",
+        "@typescript-eslint/no-unnecessary-qualifier": "error",
+        "@typescript-eslint/no-unnecessary-type-assertion": "off",
+
+		"@stylistic/quotes": [
+			"error",
+			"single",
+			{
+				"allowTemplateLiterals": true
+			}
+		],
+		"@stylistic/type-annotation-spacing": "error",
+
+
+		// JS base rules
+		"brace-style": ["error", "1tbs"],
+		"id-blacklist": "off",
+		"id-match": "off",
+		"max-len": [
+			"error",
+			{
+				ignorePattern: "^import [^,]+ from |^export | implements",
+				code: 150,
+			},
+		],
+		"no-underscore-dangle": "off",
+		"jsdoc/newline-after-description": "off",
+		"object-curly-spacing": [
+			"warn",
+			"always"
+		],
+        "no-mixed-spaces-and-tabs": "error",
+        "import/order": "error",
+        "import/no-unused-modules": "warn",
+        "@typescript-eslint/require-await": "error",
+		},
+	},
+	{
+		files: ["**/*.html"],
+		extends: [
+		...angular.configs.templateRecommended,
+		...angular.configs.templateAccessibility,
+		],
+		rules: {},
+	}
 );
