e63a4696f874bf0a57a8d0133a663e07bd77c303
tamibalogh <balogh.tamara.sara@gmail.com>
2025-09-01 15:02:57 +0200

    #EX-85 Delete CORS configurer, add /api to path

--- backend/exence/src/main/java/com/exence/finance/common/config/WebConfig.java
+++ backend/exence/src/main/java/com/exence/finance/common/config/WebConfig.java
@@ -8,17 +8,4 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig {
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200") // Angular dev server
-                        .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
-            }
-        };
-    }
 }
