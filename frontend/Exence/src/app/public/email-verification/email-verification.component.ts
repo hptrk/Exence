8cd0daefb0d88e2b52b31abd53c347da948ccb7c
Tamás Nagy <ntamasalbert@gmail.com>
2025-12-03 21:36:34 +0100

    Add email-verification.component

---
+++ frontend/Exence/src/app/public/email-verification/email-verification.component.ts
@@ -0,0 +1,56 @@
+import { Component, inject, OnInit } from "@angular/core";
+import { MatCardModule } from "@angular/material/card";
+import { ButtonComponent } from "../../shared/button/button.component";
+import { MatIconModule } from "@angular/material/icon";
+import { Router } from "@angular/router";
+import { NavigationService } from "../../shared/navigation/navigation.service";
+import { AuthService } from "../auth.service";
+import { EmailVerificationRequest } from "../../data-model/modules/auth/EmailVerificationRequest";
+
+@Component({
+	selector: '',
+	template: `
+		<mat-card class="d-flex flex-column justify-content-center align-items-center gap-4 p-5">
+			<mat-card-content class="d-flex flex-row flex-nowrap gap-4 w-100 p-0">
+				<div>
+					<div class="icon-container">
+						<mat-icon size="lg">mail</mat-icon>
+					</div>
+				</div>
+
+				<div>
+					<h2 class="fs-1 fw-bold">Email verified</h2>
+					<p class="m-0 mb-4 subtitle">
+						Congratulations! Your email has been verified. You can now use all features of <strong>Exence</strong>.
+					</p>
+					<ex-button
+						outline
+						matIcon="keyboard_backspace"
+						(click)="navigateToLogin()"
+					>
+						Back to login
+					</ex-button>
+				</div>
+			</mat-card-content>
+			
+		</mat-card>
+	`,
+	styleUrl: './email-verification.component.scss',
+	imports: [MatCardModule, MatIconModule, ButtonComponent]
+})
+export class EmailVerificationComponent implements OnInit {
+	private readonly router = inject(Router);
+	private readonly navigate = inject(NavigationService);
+	private readonly authService = inject(AuthService);
+
+	async ngOnInit(): Promise<void> {
+		const token = this.router.routerState.root.snapshot.queryParams['token'];
+		const request: EmailVerificationRequest = { token };
+		await this.authService.verifyEmail(request);
+		// TODO snackbar
+	}
+
+	navigateToLogin(): void {
+		this.router.navigateByUrl(this.navigate.account().login());
+	}
+}
--- frontend/Exence/src/app/public/public.routes.ts
+++ frontend/Exence/src/app/public/public.routes.ts
@@ -3,6 +3,7 @@ import { PublicComponent } from "./public.component";
 import { LoginComponent } from "./login/login.component";
 import { RegistrationComponent } from "./registration/registration.component";
 import { ForgotPasswordComponent } from "./forgot-password/forgot-password.component";
+import { EmailVerificationComponent } from "./email-verification/email-verification.component";
 
 export const publicRoutes: Routes = [{
 	path: 'public',
@@ -11,5 +12,6 @@ export const publicRoutes: Routes = [{
 		{ path: 'login', component: LoginComponent },
 		{ path: 'register', component: RegistrationComponent },
 		{ path: 'forgot-password', component: ForgotPasswordComponent },
+		{ path: 'verify-email', component: EmailVerificationComponent }
 	]
 }];
