e9f19d002c9141d3e396b16097ea4b71978810c7
Horánszki Patrik <horanszkipatrik@gmail.com>
2025-12-10 20:11:11 +0100

    edit proxy config to accept environment variable backend url

--- frontend/Exence/proxy.conf.js
+++ frontend/Exence/proxy.conf.js
@@ -1,7 +1,7 @@
 const PROXY_CONFIG = [
 	{
 		context: ['/api'],
-		target: 'http://localhost:8080',
+		target: process.env['API_URL'] || 'http://localhost:8080',
 		secure: false,
 		changeOrigin: true,
 		logLevel: 'debug',
