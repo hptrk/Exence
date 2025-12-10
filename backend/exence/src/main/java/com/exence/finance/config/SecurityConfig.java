71154ae772b632cdf2ed2f9d94148ab60199164b
Horánszki Patrik <horanszkipatrik@gmail.com>
2025-12-10 20:24:28 +0100

    test cors config for deployment

--- backend/exence/src/main/java/com/exence/finance/config/SecurityConfig.java
+++ backend/exence/src/main/java/com/exence/finance/config/SecurityConfig.java
@@ -50,7 +50,7 @@ public class SecurityConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("http://localhost:4200");
+        config.addAllowedOrigin("*"); // test for deployment
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
         source.registerCorsConfiguration("/**", config);
