d90120225c13375d5a1b7f01c5739c6cd7742221
Tamás Nagy <71514605+ntamasa@users.noreply.github.com>
2026-01-21 19:47:40 +0100

    #EX-252: Fix emoji propagation bug

--- frontend/Exence/src/app/private/transactions-and-categories/create-category-dialog/create-category-dialog.component.ts
+++ frontend/Exence/src/app/private/transactions-and-categories/create-category-dialog/create-category-dialog.component.ts
@@ -16,6 +16,7 @@ import { ValidatorComponent } from '../../../shared/validator/validator.componen
 import { CategoryService } from '../../category.service';
 import { AutoTrimDirective } from '../../../shared/auto-trim.directive';
 import { ConfirmExitDialogDirective } from '../../../shared/confirm-exit-dialog.directive';
+import { StopPropagationDirective } from "src/app/shared/stop-propagation.directive";
 
 @Component({
 	selector: 'ex-create-category-dialog',
@@ -34,6 +35,7 @@ import { ConfirmExitDialogDirective } from '../../../shared/confirm-exit-dialog.
 		DialogCardComponent,
 		AutoTrimDirective,
 		ConfirmExitDialogDirective,
+		StopPropagationDirective,
 	],
 })
 export class CreateCategoryDialogComponent extends DialogComponent<undefined, boolean> {
