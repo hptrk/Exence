0994fa4c9f0c25a8e9c074f2ff31f4b09fbd6ace
tamibalogh <balogh.tamara.sara@gmail.com>
2026-01-22 21:32:47 +0100

    EX-193: Change getters to computed values, input parameter name

--- frontend/Exence/src/app/private/dashboard/categories/categories.component.ts
+++ frontend/Exence/src/app/private/dashboard/categories/categories.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, input, output } from '@angular/core';
+import { Component, computed, inject, input } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { CategorySummaryResponse } from '../../../data-model/modules/category/CategorySummaryResponse';
@@ -7,8 +7,8 @@ import { ButtonComponent } from '../../../shared/button/button.component';
 import { DialogService } from '../../../shared/dialog/dialog.service';
 import { DisplaySizeService } from '../../../shared/display-size.service';
 import { CreateCategoryDialogComponent } from '../../transactions-and-categories/create-category-dialog/create-category-dialog.component';
-import { CreateTransactionDialogComponent, CreateTransactionDialogData } from '../../transactions-and-categories/create-transaction-dialog/create-transaction-dialog.component';
-import { Transaction } from 'src/app/data-model/modules/transaction/Transaction';
+import { CreateTransactionDialogComponent } from '../../transactions-and-categories/create-transaction-dialog/create-transaction-dialog.component';
+import { Category } from 'src/app/data-model/modules/category/Category';
 
 // TODO move to interval filter component when created
 export enum DateInterval {
@@ -37,8 +37,11 @@ export class CategoriesComponent extends BaseComponent {
 	private readonly dialog = inject(DialogService);
 
 	totalExpense = input.required<number>();
-	categorySummaries = input.required<CategorySummaryResponse[]>();
+	topCategories = input.required<CategorySummaryResponse[]>();
 	categories = input.required<Category[]>();
+
+	hasCategories = computed(() => !!this.categories().length)
+	hasTransactions = computed(() => !!this.topCategories().length)
 
 	async openCreateCategoryDialog(): Promise<void> {
 		await this.dialog.openNonModal(
@@ -46,28 +49,13 @@ export class CategoriesComponent extends BaseComponent {
 		);
 	}
 
-	public openCreateTransactionDialog(): void {
-			this.dialog.open<CreateTransactionDialogComponent, CreateTransactionDialogData, Transaction>(
-				CreateTransactionDialogComponent, undefined
-			).afterClosed().subscribe(
-				 (newTransaction?: Transaction) => {
-					if (newTransaction) {
-						this.snackbarService.showSuccess(`Transaction '${newTransaction.title.slice(0, 10)}${newTransaction.title.length > 10 ? '...' : ''}' created successfully!`);
-						this.dataChangedEvent.emit();
-					}
-				}
-			);
-		}
+	public async openCreateTransactionDialog(): Promise<void> {
+		await this.dialog.openNonModal(
+			CreateTransactionDialogComponent, undefined
+		);
+	}
 
 	calcPercentage(amount: number): number {
 		return Math.floor((amount / this.totalExpense()) * 100);
-	}
-
-	get hasCategories(): boolean {
-		return this.categories().length > 0;
-	}
-
-	get hasTransactions(): boolean {
-		return this.categorySummaries().length > 0;
 	}
 }
