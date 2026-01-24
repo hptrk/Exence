67ca9c08d75ef39135b2dc0830550cb34ecb982f
Tamás Nagy <71514605+ntamasa@users.noreply.github.com>
2026-01-24 20:03:51 +0100

    #EX-213: Add search option for category select menu
    * #EX-213: Add search option for category select menu
    
    * Fix category search not reseting after select close
    
    Resulted in no dropdown if closed when no results were found.

--- frontend/Exence/src/app/private/transactions-and-categories/create-transaction-dialog/create-transaction-dialog.component.ts
+++ frontend/Exence/src/app/private/transactions-and-categories/create-transaction-dialog/create-transaction-dialog.component.ts
@@ -1,14 +1,18 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, computed, inject, OnInit, signal } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
 import { NonNullableFormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
+import { startWith } from 'rxjs';
 import { Category } from '../../../data-model/modules/category/Category';
 import { Transaction } from '../../../data-model/modules/transaction/Transaction';
 import { TransactionType } from '../../../data-model/modules/transaction/TransactionType';
+import { AutoTrimDirective } from '../../../shared/auto-trim.directive';
 import { ButtonComponent } from '../../../shared/button/button.component';
+import { ConfirmExitDialogDirective } from '../../../shared/confirm-exit-dialog.directive';
 import { DialogCardComponent } from '../../../shared/dialog-card/dialog-card.component';
 import { DialogWithBaseComponent } from '../../../shared/dialog/dialog.service';
 import { InputClearButtonComponent } from '../../../shared/input-clear-button/input-clear-button.component';
@@ -16,8 +20,6 @@ import { SnackbarService } from '../../../shared/snackbar/snackbar.service';
 import { ValidatorComponent } from '../../../shared/validator/validator.component';
 import { CategoryService } from '../../category.service';
 import { TransactionService } from '../transaction.service';
-import { AutoTrimDirective } from '../../../shared/auto-trim.directive';
-import { ConfirmExitDialogDirective } from '../../../shared/confirm-exit-dialog.directive';
 
 export interface CreateTransactionDialogData {
 	type?: TransactionType;
@@ -51,9 +53,7 @@ export class CreateTransactionDialogComponent extends DialogWithBaseComponent<Cr
 
 	data = this.dialogRef.value;
 
-	transactionTypes: TransactionType[] = Object.values(TransactionType);
-
-	categories: Category[] = [];
+	transactionTypes: TransactionType[] = Object.values(TransactionType);	
 
 	form = this.fb.group({
 		title: this.fb.control<string>('', [Validators.required, Validators.maxLength(255)]),
@@ -62,11 +62,26 @@ export class CreateTransactionDialogComponent extends DialogWithBaseComponent<Cr
 		amount: this.fb.control<number | null>(null, [Validators.required, Validators.min(1)]),
 		type: this.fb.control<TransactionType>(TransactionType.EXPENSE, [Validators.required]),
 		recurring: this.fb.control<boolean>(false),
-		category: this.fb.control<Category | null>(null, [Validators.required]),
+		category: this.fb.group({
+			category: this.fb.control<Category | null>(null, [Validators.required]),
+			searchText: this.fb.control<string>('', [Validators.maxLength(25)]),
+		}),
+	});
+
+	private categories = signal<Category[]>([]);
+	private searchText = toSignal(this.form.controls.category.controls.searchText.valueChanges.pipe(startWith('')), { initialValue: '' });
+	
+	filteredCategories = computed(() => {
+		const categories = this.categories();
+		const search = this.searchText();
+		if (!search) return categories;
+		return categories.filter(category => 
+			category.name.toLowerCase().includes(search.toLowerCase())
+		);
 	});
 
 	async ngOnInit(): Promise<void> {
-		this.categories = await this.categoryService.list();
+		this.categories.set(await this.categoryService.list());
 		if (this.data?.type) {
 			this.form.controls.type.setValue(this.data.type);
 		}
@@ -93,7 +108,7 @@ export class CreateTransactionDialogComponent extends DialogWithBaseComponent<Cr
 			amount: formValue.amount!,
 			type: formValue.type,
 			recurring: formValue.recurring,
-			categoryId: formValue.category!.id!,
+			categoryId: formValue.category!.category!.id!,
 		} as Transaction;
 		try {
 			const newTransaction = await this.transactionService.create(request);
