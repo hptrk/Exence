be921322b730fd97d4b846895edfdc1e609f5f6f
Tamás Nagy <ntamasalbert@gmail.com>
2026-02-21 22:35:27 +0100

    Rename statistic-card-list to stat-card-list

--- frontend/Exence/src/app/private/statistics/stat-card-list/stat-card-list.component.ts
+++ frontend/Exence/src/app/private/statistics/stat-card-list/stat-card-list.component.ts
@@ -2,11 +2,11 @@ import { Component, input } from '@angular/core';
 import { EmptyStatisticCardComponent } from '../empty-statistic-card.component';
 
 @Component({
-	selector: 'ex-statistic-card-list',
-	templateUrl: './statistic-card-list.component.html',
-	styleUrl: './statistic-card-list.component.scss',
+	selector: 'ex-stat-card-list',
+	templateUrl: './stat-card-list.component.html',
+	styleUrl: './stat-card-list.component.scss',
 	imports: [EmptyStatisticCardComponent],
 })
-export class StatisticCardListComponent {
+export class StatCardListComponent {
 	numberOfCards = input.required<number>();
 }
