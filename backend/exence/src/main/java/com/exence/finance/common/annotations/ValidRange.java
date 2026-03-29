4bec565b3bc90e9ac3e626b77cfd8c4cf5092f3e
Horánszki Patrik <71675786+hptrk@users.noreply.github.com>
2026-03-29 13:19:11 +0200

    EX-326: Make general Comparable range validator with tests (#108)

--- backend/exence/src/main/java/com/exence/finance/common/annotations/ValidRange.java
+++ backend/exence/src/main/java/com/exence/finance/common/annotations/ValidRange.java
@@ -1,32 +1,41 @@
 package com.exence.finance.common.annotations;
 
-import com.exence.finance.common.validators.DateRangeValidator;
+import com.exence.finance.common.validators.RangeValidator;
 import jakarta.validation.Constraint;
 import jakarta.validation.Payload;
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Repeatable;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Target({ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
-@Constraint(validatedBy = DateRangeValidator.class)
+@Constraint(validatedBy = RangeValidator.class)
 @Documented
-public @interface ValidDateRange {
-    String message() default "{validation.date-range.invalid}";
+@Repeatable(ValidRange.List.class)
+public @interface ValidRange {
+    String message() default "{validation.range.invalid}";
 
     Class<?>[] groups() default {};
 
     Class<? extends Payload>[] payload() default {};
 
     /**
-     * Field name of the start date
+     * Field name of the range start (lower bound)
      */
     String from();
 
     /**
-     * Field name of the end date
+     * Field name of the range end (upper bound)
      */
     String to();
+
+    @Target({ElementType.TYPE})
+    @Retention(RetentionPolicy.RUNTIME)
+    @Documented
+    @interface List {
+        ValidRange[] value();
+    }
 }
