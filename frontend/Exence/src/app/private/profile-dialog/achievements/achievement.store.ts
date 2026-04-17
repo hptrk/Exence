04f93be78b202137a41d37820a1704124db56e27
Tamás Nagy <ntamasalbert@gmail.com>
2026-04-17 20:39:11 +0200

    NOTICKET: Fix achievements workspace syncing

--- frontend/Exence/src/app/private/profile-dialog/achievements/achievement.store.ts
+++ frontend/Exence/src/app/private/profile-dialog/achievements/achievement.store.ts
@@ -1,16 +1,20 @@
 import { computed, inject, resource } from '@angular/core';
 import { signalStore, withComputed, withMethods, withProps } from '@ngrx/signals';
 import { AchievementGet, UserAchievementGet } from '../../../data-model/modules/achievement';
+import { WorkspaceGet } from '../../../data-model/modules/workspaces/WorkspaceGet';
+import { WorkspaceService } from '../../../shared/workspace.service';
 import { AchievementService } from './achievement.service';
 
 export const AchievementStore = signalStore(
 	{ providedIn: 'root' },
 
-	withProps((_, service = inject(AchievementService)) => ({
-		allResource: resource<AchievementGet[], void>({
+	withProps((_, service = inject(AchievementService), workspaceService = inject(WorkspaceService)) => ({
+		allResource: resource<AchievementGet[], WorkspaceGet | null>({
+			params: () => workspaceService.currentWorkspace(),
 			loader: () => service.list(),
 		}),
-		unlockedResource: resource<UserAchievementGet[], void>({
+		unlockedResource: resource<UserAchievementGet[], WorkspaceGet | null>({
+			params: () => workspaceService.currentWorkspace(),
 			loader: () => service.listUnlocked(),
 		}),
 	})),
